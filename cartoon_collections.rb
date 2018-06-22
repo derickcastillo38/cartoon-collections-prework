4d857ba568c91b3c9cf96068e2101ba9f9efc424
derickcastillo38 <9derick@gmail.com>
2018-06-22 02:44:47 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
-  dwarves.map.with_index do |d, idx|
-    puts "#{idx}. #{d}"
+  dwarves.map.with_index do |name, idx|
+    puts "#{idx}. #{name}"
   end
 end
 
